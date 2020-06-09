92910b5e40fcc1a4b8f647eef29457fe80d0f1b3
ChairsDaily <chairs.daily.2019@gmail.com>
2020-06-09 16:40:51 -0400

    update

---
+++ src/tinysh.h
@@ -0,0 +1,7 @@
+#define TOKEN_BUFSIZE       64
+#define ALLOC_ERROR         "allocation error\n"
+#define EXEC_ERROR          "execution error\n"
+#define TOKEN_DELIM         " \t\r\n\a"
+#define RUNNING             1
+#define PROGRAM_STRING         "Tiny.sh Version 0.1"
+#define COPY_STRING            "Copyleft (C) 2020 Kaleb H."
